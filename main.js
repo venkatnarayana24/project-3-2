bf017b3f51f47b14af0ca8801c6ed2fcb357af45
venkatnarayana24 <132664296+venkatnarayana24@users.noreply.github.com>
2024-03-08 14:28:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function openPage(url) {
+    // Specify the URL of the page you want to open
+   // Change this to the desired URL
+    // Open the URL in the current tab
+    
+    
+    setTimeout(function() {
+        window.location.href = url + ".html"; // Redirect to the specified URL after delay
+    }, 1000); 
+}
+ 
