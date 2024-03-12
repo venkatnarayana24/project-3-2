1668145d3885e6a54eccd6bae20cdb22342f4f06
venkatnarayana24 <132664296+venkatnarayana24@users.noreply.github.com>
2024-03-12 17:33:49 +0530

    main.js

--- main.js
+++ main.js
@@ -9,4 +9,20 @@ function openPage(url) {
         window.location.href = "https://raw.githack.com/venkatnarayana24/project-3-2/main/" +url + ".html"; // Redirect to the specified URL after delay
     
 }
+// JavaScript to enable scrolling without scrollbar
+
+// Function to handle mouse wheel event
+function handleMouseWheel(event) {
+    const delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
+    document.documentElement.scrollTop -= (delta * 20);
+    document.body.scrollTop -= (delta * 20);
+    event.preventDefault();
+}
+
+// Event listener for mouse wheel
+if (window.addEventListener) {
+    window.addEventListener('DOMMouseScroll', handleMouseWheel, false);
+}
+window.onmousewheel = document.onmousewheel = handleMouseWheel;
+
  
